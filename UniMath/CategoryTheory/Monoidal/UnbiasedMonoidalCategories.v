e3d0763077f083ae5e76e2d28858b65947c9da62
tslil clingman <>
2019-04-05 13:06:01 +0100

    Working towards the correct diagram

--- UniMath/CategoryTheory/Monoidal/UnbiasedMonoidalCategories.v
+++ UniMath/CategoryTheory/Monoidal/UnbiasedMonoidalCategories.v
@@ -92,34 +92,6 @@ Section ListPrecategory.
     - exact (precategory_binproduct C IHn).
   Defined.
 
-  Definition horizontal_domain : ∏ (l : list nat), precategory.
-  Proof.
-    use list_ind.
-    - exact unit_category.
-    - intros k ks partial_product.
-      exact (precategory_binproduct (list_precategory k) partial_product).
-  Defined.
-
-  (** Given [l : list nat ], this constructs the cannonical functor from
-
-      C^l(0) x (C^l(1) x .. (C^l(last))) --> C^(sumlist l)
-
-   *)
-
-  Definition flatten_functor : ∏ (l : list nat),
-    functor (horizontal_domain l) (list_precategory (sumlist l)).
-  Proof.
-    use list_ind.
-    - exact (functor_identity unit_category).
-    - intros k ks suffix_functor.
-      induction k.
-      + exact (lunit_functor _ ∙ suffix_functor).
-      + refine (functor_composite _ _).
-        exact (precategory_binproduct_unassoc
-                 C (list_precategory k) (horizontal_domain ks)).
-        exact (pair_functor (functor_identity C) IHk).
-  Defined.
-
 End ListPrecategory.
 
 Section UnivalentListCategory.
@@ -142,44 +114,65 @@ Section UnbiasedMoniodalPrecat.
 
   (** An unbiased (lax) monoidal category is a category C equipped with maps:
 
-      tensor_n : C^n -> C
+      ⊗_n : C^n -> C
 
       for each natural n, as well as natural transformations
 
-      γ : tensor_k ∘ (tensor_n1 , ... , tensor_nk) -> tensor_(∑ n_i)
-      ι : id_C -> tensor_1
+      γ : ⊗_k ∘ (⊗_n1 , ... , ⊗_nk) -> ⊗_(∑ n_i)
+      ι : id_C -> ⊗_1
 
       for each k ∈ ℕ, and n_1, ..., n_k ∈ ℕ, subject to the following.
    *)
 
   Context {C : precategory} (tensor : ∏ n, list_precategory C n ⟶ C) .
 
-  (** This constructs, for [ l: list nat ], the functor
+  Local Notation "⊗ n" := (tensor n) (at level 30).
 
-      C^l(0) x (C^l(1) ... ( C^l(last) )) --> C^(length l)
+  (** This constructs (C^l(0)) x .. x (C^l(last))  *)
+  Definition h_dom : ∏ (l : list nat), precategory.
+  Proof.
+    use list_ind.
+    - exact unit_category.
+    - intros k ks partial_product.
+      exact (precategory_binproduct (list_precategory C k) partial_product).
+  Defined.
 
-      by taking tensors on each listed power.
+  (** This is the horizontal product of tensor functors,
+      (C^l(0)) x .. x (C^l(last))
+          |               |
+          | ⊗  x .. x   ⊗ |
+          v               v
+          C x    ..     x C
    *)
-
-  Definition horizontal_tensor : ∏ (l : list nat),
-    functor (horizontal_domain C l) (list_precategory C (length l)).
+  Definition h_ten : ∏ (l : list nat),
+    h_dom l ⟶ list_precategory C (length l).
   Proof.
     use list_ind.
     - exact (functor_identity unit_category).
     - intros k ks functor.
-      exact (pair_functor (tensor k) functor).
+      exact (pair_functor (⊗ k) functor).
   Defined.
 
-  Definition gammator (l : list nat) : UU
-    := nat_trans ((horizontal_tensor _) ∙ (tensor (length l)))
-                 ((flatten_functor _ _) ∙ tensor (sumlist l)).
+  (** Given [l : list nat ], this constructs the cannonical functor from
 
-  Definition iotator : UU
-    := nat_trans ((runit_inv_functor C) ∙ tensor 1) (functor_identity _).
+      C^l(0) x (C^l(1) x .. (C^l(last))) --> C^(sumlist l)
 
-  Section first_functor.
+   *)
+  Definition flatten_functor : ∏ (l : list nat),
+      h_dom l ⟶ list_precategory C (sumlist l).
+  Proof.
+    use list_ind.
+    - exact (functor_identity unit_category).
+    - intros k ks suffix_functor.
+      induction k.
+      + exact (lunit_functor _ ∙ suffix_functor).
+      + refine (functor_composite _ _).
+        exact (precategory_binproduct_unassoc
+                 C (list_precategory C k) (h_dom ks)).
+        exact (pair_functor (functor_identity C) IHk).
+  Defined.
 
-      (** Here we construct
+  (** Here we construct
 
                        (C^(k_(1,1)) x ... x C^(k_(1,j_1)))    -
                                                                |
@@ -194,40 +187,35 @@ Section UnbiasedMoniodalPrecat.
                                              j_i = length ll[i]
                                          k_(i,m) = ll[i][m]
 
-      as well as the functor (below) with this domain and codomain C.
-
-         x_n (x_m1 (x_k11) ... (x_k1j1) )
-             (x_m2 (x_k21 ) ...(x_k2j2) ) ...
-             (x_mn (x_kn1) ... (x_knjn) )
-
+    This is the horizontal, horizontal domain.
    *)
 
-    Definition double_list_to_domain1 : ∏ (ll : list (list nat)), precategory.
+    Definition hh_dom : ∏ (ll : list (list nat)), precategory.
     Proof.
       use list_ind.
       - exact unit_category.
       - intros l ls IHls.
-        exact (precategory_binproduct (horizontal_domain C l) IHls).
+        exact (precategory_binproduct (h_dom l) IHls).
     Defined.
 
     Definition functor_layer1 :=
-      ∑ (l : list nat), horizontal_domain C l ⟶ list_precategory C (length l).
+      ∑ (l : list nat), h_dom l ⟶ list_precategory C (length l).
 
     Definition functor_layer2 :=
       ∑ (ll : list (list nat)),
-      double_list_to_domain1 ll ⟶ horizontal_domain C (map length ll).
+      hh_dom ll ⟶ h_dom (map length ll).
 
     Definition functor_layer3 := ∑ (ll : list (list nat)),
-      double_list_to_domain1 ll ⟶ list_precategory C (length ll).
+      hh_dom ll ⟶ list_precategory C (length ll).
 
-    Definition list_of_tensors : list (list nat) → list functor_layer1.
+    Definition list_of_tensor : list (list nat) → list functor_layer1.
     Proof.
       apply map.
       intro l.
-      exact (l ,, horizontal_tensor _).
+      exact (l ,, h_ten _).
     Defined.
 
-    Definition innermost_tensor_functor : list functor_layer1 -> functor_layer2.
+    Definition innermost_tensor : list functor_layer1 -> functor_layer2.
     Proof.
       apply foldr.
       - intros lF llG.
@@ -236,8 +224,8 @@ Section UnbiasedMoniodalPrecat.
       - exact (nil ,, functor_identity _).
     Defined.
 
-    Definition innermost_tensor_list_preserves_shape : ∏ (ll : list (list nat)),
-      pr1 (innermost_tensor_functor (list_of_tensors ll)) = ll.
+    Definition innermost_tensor_preserves_list_shape : ∏ (ll : list (list nat)),
+      pr1 (innermost_tensor (list_of_tensor ll)) = ll.
     Proof.
       use list_ind.
       - reflexivity.
@@ -245,17 +233,61 @@ Section UnbiasedMoniodalPrecat.
         exact (maponpaths (cons l) IH).
     Defined.
 
-    Definition pre_tensor1 {ll : list (list nat)} :
-      double_list_to_domain1 ll ⟶ horizontal_domain C (map length ll).
+    (** This constructs the functor
+
+            (C^(k_(1,1)) x ... x C^(k_(1,j_1)))
+          x ...
+          x (C^(k_(n,1)) x ... x C^(k_(n,j_n)))
+
+                           |
+                           | (⊗ x .. x ⊗) x .. x (⊗ x .. x ⊗)
+                           |
+                           v
+
+                     (C x ... x C)
+                   x ...
+                   x (C x ... x C)
+
+     *)
+    Definition hh_ten {ll : list (list nat)} :
+      hh_dom ll ⟶ h_dom (map length ll).
     Proof.
-      apply (transportf (fun ll => functor (double_list_to_domain1 ll)
-                                        (horizontal_domain C (map length ll)))
-                        (innermost_tensor_list_preserves_shape ll)).
-      exact (pr2 (innermost_tensor_functor (list_of_tensors ll))).
+      apply (transportf (fun ll => functor (hh_dom ll)
+                                        (h_dom (map length ll)))
+                        (innermost_tensor_preserves_list_shape ll)).
+      exact (pr2 (innermost_tensor (list_of_tensor ll))).
     Defined.
 
+  (** This constructs, for [ l: list nat ], the functor
+
+      C^l(0) x (C^l(1) ... ( C^l(last) )) --> C^(length l)
+
+      by taking tensors on each listed power.
+   *)
+
+
+  Definition gammator (l : list nat) : UU
+    := nat_trans ((h_ten _) ∙ (⊗ (length l)))
+                 ((flatten_functor _ _) ∙ ⊗ (sumlist l)).
+
+  Definition iotator : UU
+    := nat_trans ((runit_inv_functor C) ∙ ⊗ 1) (functor_identity _).
+
+  Section first_functor.
+
+
+    (**
+      as well as the functor (below) with this domain and codomain C.
+
+         x_n (x_m1 (x_k11) ... (x_k1j1) )
+             (x_m2 (x_k21 ) ...(x_k2j2) ) ...
+             (x_mn (x_kn1) ... (x_knjn) )
+
+
+  *)
+
     Definition tensor1 {ll : list (list nat)} : double_list_to_domain1 ll ⟶ C
-      := pre_tensor1 ∙ horizontal_tensor _ ∙ tensor _.
+      := pre_tensor1 ∙ h_ten _ ∙ ⊗ _.
   End first_functor.
 
   Section second_functor.
@@ -272,18 +304,18 @@ Section UnbiasedMoniodalPrecat.
              (x_(sum xkni) )
      *)
 
-    Definition double_list_to_domain2 (ll : list (list nat)) : precategory
-      := horizontal_domain C (flattenlist ll).
+    Definition double_list_to_domain3 (ll : list (list nat)) : precategory
+      := horizontal_domain C (map length ll).
 
-    Definition tensor2 {ll : list (list nat)} : double_list_to_domain2 ll ⟶ C
-      := horizontal_tensor _ ∙ tensor _.
+    Definition tensor2 {ll : list (list nat)} : double_list_to_domain3 ll ⟶ C
+      := h_ten _ ∙ ⊗ _.
 
   End second_functor.
 
   Section third_functor.
 
     (** In the established notation, given [ ll: list (list nat) ] this section
-        constructs
+        constructsn
 
         C^(sum k_(1,i)) x ( ... x C^(sum k_(n,i)))
 
@@ -293,28 +325,28 @@ Section UnbiasedMoniodalPrecat.
 
      *)
 
-  Definition double_list_to_domain3 (ll : list (list nat)) : precategory
+  Definition double_list_to_domain4 (ll : list (list nat)) : precategory
     := horizontal_domain C (map sumlist ll).
 
-  Definition tensor3 {ll : list (list nat)} : double_list_to_domain3 ll ⟶ C
-    := horizontal_tensor _ ∙ tensor _ .
+  Definition tensor3 {ll : list (list nat)} : double_list_to_domain4 ll ⟶ C
+    := h_ten _ ∙ ⊗ _ .
 
   End third_functor.
 
   Section fourth_functor.
 
-    (** Given [ ll: list (list nat) ] this section constructs the tensor functor
+    (** Given [ ll: list (list nat) ] this section constructs the ⊗ functor
 
 
                             x_(sum sum (k_ij))
         C^(sum sum k_(i,j)) -------------------> C
      *)
 
-  Definition double_list_to_domain4 (ll : list (list nat)) : precategory
+  Definition double_list_to_domain5 (ll : list (list nat)) : precategory
     := list_precategory C (sumlist (map sumlist ll)).
 
-  Definition tensor4 {ll : list (list nat)} : double_list_to_domain4 ll ⟶ C
-    := tensor _.
+  Definition tensor4 {ll : list (list nat)} : double_list_to_domain5 ll ⟶ C
+    := ⊗ _.
 
   End fourth_functor.
 
@@ -322,37 +354,35 @@ Section UnbiasedMoniodalPrecat.
 
     (** In this section we construct the pasting
 
-                     tensor1
-             domain1 ---------> A
-                 |             ||
-        flatten  |  gammator   ||
-                 |             ||
-             domain1 -tensor2-> A
-                 |             ||
-        flatten  |  gammator   ||
-                 |             ||
+        This is wrong. We need to be able to commute flatten with products of functors.
+
+
+             domain1 ======== domain1
+                 |              |
+    pre_tensor1  |              |  tensor1
+                 v   h_ten ∙ ⊗  v
              domain2 ---------> A
-                    tensor3
+                 |             ||
+        flatten  |  gammator   ||
+                 v             ||
+             domain2 -tensor2-> A
+                 |             ||
+        flatten  |  gammator   ||
+                 v             ||
+             domain3 ---------> A
+                      tensor3
      *)
 
-    Context (ll : list (list nat)).
+    Variable (ll : list (list nat)).
 
-    (* Definition stuff : ∏ (ll : list (list nat)), *)
-    (*                    double_list_to_domain1 ll = *)
-    (*                    list_precategory _ _. *)
+    Definition first_whisker1 : gammator (map length ll) ->
+      tensor1 ⟹ pre_tensor1 ∙ flatten_functor _ (map length ll) ∙ ⊗ _
+      := pre_whisker pre_tensor1.
 
-    Check tensor1.
+    Check @pre_tensor1 ll.
     Check pre_tensor1 ∙ flatten_functor _ _.
 
-    Definition first_gamma : ∏ (ll : list (list nat)) (g : gammator (map sumlist ll)),
-                             tensor1 ⟹ pre_tensor1  ∙ tensor2.
-
-    Check tensor1 ll.
-    Check gammator (map sumlist ll).
-
-    Check pre_whisker.
-
-
-
-  Definition gamma_comp1 : UU :=
-    ∏ (ll : (list (list nat))), .
+    Definition second_whisker1 : gammator (map length ll) ->
+                                 pre_tensor1 ∙ flatten_functor _ (map length ll) ∙ tensor2 ⟹ pre_tensor1 ∙
+                                             flatten_functor _ _ ∙
+                                             flatten_functor _ _ ∙ tensor3.
